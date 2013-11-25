048a546a28392db7b4f5f562e0036d0b21032ed4
Alejandro Blanco <alejandro.b.e@gmail.com>
2013-11-25 20:13:59 +0100

    Redo github repos info using bootstrap popovers
    Add animations to bootstrap custom build.

--- logic/github.js
+++ logic/github.js
@@ -4,17 +4,18 @@
 jQuery.github = {
     getRepos: function (username, callback) {
         "use strict";
-        jQuery.getJSON("https://api.github.com/users/" + username + "/repos?callback=?", callback);
+        jQuery.getJSON("https://api.github.com/users/" + username +
+            "/repos?callback=?", callback);
     },
 
     filterList: [
         "config", "GECO", "geco-ffox-extension", "cagg-browser-extensions",
         "agora-ciudadana", "yith-library-server", "django-mathjax", "mensab",
-        "postgresql", "dissim", "dissim-memoria", "zc.sourcerelease"
+        "postgresql", "dissim-memoria", "zc.sourcerelease"
     ]
 };
 
-jQuery.fn.loadRepositories = function (username, descViewport) {
+jQuery.fn.loadRepositories = function (username) {
     "use strict";
     var target = this,
         sortByNumberOfWatchers;
@@ -29,46 +30,34 @@ jQuery.fn.loadRepositories = function (username, descViewport) {
 
     $.github.getRepos(username, function (response) {
         var repos = response.data,
-            descriptions = $(descViewport),
-            list = $('<ul class="list-unstyled"><ul/>'),
-            projects,
-            projectsDescs,
-            showDesc;
+            list = $('<ul class="list-unstyled"><ul/>');
 
         sortByNumberOfWatchers(repos);
-
         target.empty().append(list);
+
         $(repos).filter(function () {
             return jQuery.inArray(this.name, $.github.filterList) === -1;
-        }).each(function (idx) {
-            var name = this.name;
-            if (name.length > 25) {
-                name = name.substring(0, 22) + "...";
-            }
-            list.append("<li class='github' id='" + idx + "-project'><a href='" + this.html_url + "'>" + name + "</a> <span class='fa fa-info-circle pull-right'></span>");
-            descriptions.append("<span class='hide' id='" + idx + "-projectdesc'>" + this.description + "</span>");
-        });
+        }).each(function () {
+            var name = this.name,
+                $info,
+                $li;
 
-        // mouse behaviours
-        projects = list.find('li');
-        projectsDescs = descriptions.find('span');
+            $li = $("<li class='github'><a href='" + this.html_url + "'>" +
+                name + "</a> <span class='fa fa-info-circle pull-right'>" +
+                "</span>");
 
-        showDesc = function (node) {
-            var desc = $("#" + node.id.split('-')[0] + "-projectdesc");
-            projectsDescs.addClass("hide");
-            desc.removeClass("hide");
-        };
+            $info = $li.find("span.fa-info-circle");
+            $info.popover({
+                placement: "left",
+                trigger: "click",
+                content: this.description
+            });
 
-        projects.on("mouseenter", function () {
-            showDesc(this);
-        });
+            $info.on("show.bs.popover", function () {
+                $("#github-projects span.fa-info-circle").popover("hide");
+            });
 
-        projects.on("mouseleave", function () {
-            projectsDescs.addClass("hide");
-        });
-
-        list.find("li img.info").click(function () {
-            showDesc($(this).parent()[0]);
+            list.append($li);
         });
     });
 };
